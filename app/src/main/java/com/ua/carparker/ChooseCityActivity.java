80ec0e297d84f5454dfdffef1010fd2a0f7593c0
Alexander <sadwyn1@gmail.com>
2017-02-07 13:11:40 +0200

    first commit

---
+++ app/src/main/java/com/ua/carparker/ChooseCityActivity.java
@@ -0,0 +1,13 @@
+package com.ua.carparker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseCityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_city);
+    }
+}
